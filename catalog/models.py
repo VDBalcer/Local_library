442ada16dfd522abc29f7682e5e579cf1136b534
Vadim <VDBalcer@gmail.com>
2021-02-04 23:14:20 +0300

    Add Author model

--- catalog/models.py
+++ catalog/models.py
@@ -12,9 +12,10 @@ class Genre(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=100, help_text="Название книги")
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL(), null=True)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     summary = models.TextField(help_text="Введите краткое описание книги")
-    isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international'
+                                                             '.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField('Genre', help_text="Выберите жанр книги")
 
     def __str__(self):
@@ -25,19 +26,34 @@ class Book(models.Model):
 
 
 class BookInstance(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), help_text="Unique ID for this particular book across whole library")
-    book = models.ForeignKey('Book', on_delete=models.SET_NULL(), null=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), help_text="Unique ID for this particular book "
+                                                                            "across whole library")
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(blank=True)
     LOAN_STATUS = (
         ('m', 'Maintenance'),
         ('o', 'On loan'),
-        ('a', 'Available')
+        ('a', 'Available'),
         ('r', 'Reserved')
     )
     status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability')
+
     class Meta:
         ordering = ["due_back"]
 
     def __str__(self):
         return f'{self.id} : {self.book.name}'
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
