ff113a1472d68b5ab179c31ef4afb96e1209fdd6
Vadim <VDBalcer@gmail.com>
2021-02-06 23:19:41 +0300

    Add Inline classes for Author and BookInstance

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,19 +2,39 @@ from django.contrib import admin
 from .models import *
 
 
-# admin.site.register(Author)
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = False
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ['last_name', 'first_name', 'date_of_birth']
+    fields = ['last_name', 'first_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = False
 
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'display_genre']
     list_filter = ['genre', 'author']
+    inlines = [BookInstanceInline]
 
 
 class BookInstanceAdmin(admin.ModelAdmin):
     list_display = ['show_id', 'book', 'status']
     list_filter = ['status', 'due_back']
+    exclude = ['id']
+    fieldsets = (
+        (None, {
+            'fields': ['book', 'imprint']
+        }),
+        ('Доступность книги', {
+            'fields': ['status', 'due_back']
+        })
+    )
 
 
 admin.site.register(Author, AuthorAdmin)
